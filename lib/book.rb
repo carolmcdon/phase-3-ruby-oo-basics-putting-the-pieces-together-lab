45902374b74136ee3357aa871a9ce8cea953ec60
Caroline <ckmcdon@gmail.com>
2022-07-19 22:18:55 -0400

    commit tree bf90a1fc3d01b4e2400fe4594ea5a8b07857ec2b

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
+class Book
+    attr_reader :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    attr_accessor :turn_page
 
+    def initialize(title)
+        @title = title
+    end
 
+    def turn_page(a_string_matching)
+        @a_string_matching = "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+b1 = Book.new("And Then There Were None")
+b1.author = "Agatha Christie"
+b1.page_count = 272
+b1.genre = "Mystery"
+
+b2 = Book.new("The World According to Garp")
+b2.turn_page
